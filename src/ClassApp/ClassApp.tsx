026d490c41f19b3f38e7f1c6f46178171109770f
lilsecret <chuckm248@gmail.com>
2024-04-05 12:11:13 -0600

    add functions to class app

--- src/ClassApp/ClassApp.tsx
+++ src/ClassApp/ClassApp.tsx
@@ -1,30 +1,24 @@
 import { Component } from "react";
 import { ClassForm } from "./ClassForm";
-import { UserInformation } from "../types";
+import { TUserInformation } from "../types";
 import { ProfileInformation } from "../ProfileInformation";
-type State = { userInformation: UserInformation | null };
+type TState = { userInformation: TUserInformation | null };
 
-const defaultUser: UserInformation = {
-  email: "default@default.com",
-  firstName: "Default",
-  lastName: "Default",
-  phone: "1234567",
-  city: "Hobbiton",
-};
+export class ClassApp extends Component<Record<string, never>, TState> {
+  state = {
+    userInformation: null,
+  };
 
-export class ClassApp extends Component<Record<string, never>, State> {
+  setUserInformation = (userInfo: TUserInformation) => {
+    this.setState({ userInformation: userInfo });
+  };
+
   render() {
     return (
       <>
         <h2>Class</h2>
-        <ProfileInformation
-          userData={
-            // toggle the following lines to change
-            // null
-            defaultUser
-          }
-        />
-        <ClassForm />
+        <ProfileInformation userData={this.state.userInformation} />
+        <ClassForm setUserInformation={this.setUserInformation} />
       </>
     );
   }
--- src/ClassApp/ClassForm.tsx
+++ src/ClassApp/ClassForm.tsx
@@ -1,5 +1,16 @@
 import { Component } from "react";
 import { ErrorMessage } from "../ErrorMessage";
+import { TUserInformation } from "../types";
+import { isEmailValid } from "../utils/validations";
+import { allCities } from "../utils/all-cities";
+
+type TErrorCheckInputs =
+  | "all"
+  | "firstName"
+  | "lastName"
+  | "email"
+  | "city"
+  | "phoneNumber";
 
 const firstNameErrorMessage = "First name must be at least 2 characters long";
 const lastNameErrorMessage = "Last name must be at least 2 characters long";
@@ -7,10 +18,112 @@ const emailErrorMessage = "Email is Invalid";
 const cityErrorMessage = "State is Invalid";
 const phoneNumberErrorMessage = "Invalid Phone Number";
 
-export class ClassForm extends Component {
+type TClassProps = {
+  setUserInformation: (userInfo: TUserInformation) => void;
+};
+
+const phoneNumberLengths = [2, 2, 2, 1];
+
+export class ClassForm extends Component<TClassProps> {
+  state = {
+    singleInputs: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      city: "",
+    },
+    phoneNumber: ["", "", "", ""],
+    isFormSubmitted: false,
+    firstNameError: false,
+    lastNameError: false,
+    emailError: false,
+    cityError: false,
+    phoneNumberError: false,
+  };
+
+  updateStateOnSingleInputs = (
+    property: keyof typeof this.state.singleInputs,
+    value: string
+  ) => {
+    const updatedObject = { ...this.state.singleInputs };
+
+    updatedObject[property] = value;
+    this.setState({ singleInputs: updatedObject });
+  };
+
+  errorCheckAllInputs = () => {
+    const { firstName, lastName, email, city } = this.state.singleInputs;
+    const { phoneNumber } = this.state;
+    const inputConditions = [
+      firstName.length <= 1,
+      lastName.length <= 1,
+      !isEmailValid(email),
+      !allCities.includes(city),
+      phoneNumber.join("").length < 7,
+    ];
+
+    this.setState({ setIsFormSubmitted: true });
+    this.setState({ firstNameError: inputConditions[0] });
+    this.setState({ lastNameError: inputConditions[1] });
+    this.setState({ emailError: inputConditions[2] });
+    this.setState({ cityError: inputConditions[3] });
+
+    if (inputConditions.includes(true)) {
+      alert("You have entered a bad form");
+      return true;
+    }
+    return false;
+  };
+
+  errorCheckInput = (input: TErrorCheckInputs, value: string) => {
+    if (this.state.isFormSubmitted) {
+      const { phoneNumber } = this.state;
+      if (input === "firstName") {
+        this.setState({ firstNameError: value.length <= 1 });
+      }
+      if (input === "lastName") {
+        this.setState({ lastNameError: value.length <= 1 });
+      }
+      if (input === "email") {
+        this.setState({ emailError: !isEmailValid(value) });
+      }
+      if (input === "city") {
+        this.setState({ cityError: !allCities.includes(value) });
+      }
+      // if (input === "phoneNumber") {
+      //   this.setState({phoneNumberError: phoneNumber.join("").length < 7});
+      // }
+    }
+  };
+
+  updateUserInformation = (boolean: boolean) => {
+    if (boolean) {
+      const validUserInformation = {
+        ...this.state.singleInputs,
+        phone: this.state.phoneNumber.join(""),
+      };
+      this.props.setUserInformation(validUserInformation);
+    }
+  };
+
   render() {
+    const {
+      firstNameError,
+      lastNameError,
+      emailError,
+      cityError,
+      phoneNumberError,
+    } = this.state;
+
     return (
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          this.setState({ isFormSubmitted: true });
+          const caughtErrors = this.errorCheckAllInputs();
+          this.updateUserInformation(caughtErrors);
+        }}
+      >
         <u>
           <h3>User Information Form</h3>
         </u>
@@ -18,30 +131,60 @@ export class ClassForm extends Component {
         {/* first name input */}
         <div className="input-wrap">
           <label>{"First Name"}:</label>
-          <input placeholder="Bilbo" />
+          <input
+            placeholder="Bilbo"
+            onChange={(e) => {
+              this.updateStateOnSingleInputs("firstName", e.target.value);
+              this.errorCheckInput("firstName", e.target.value);
+            }}
+          />
         </div>
-        <ErrorMessage message={firstNameErrorMessage} show={true} />
+        <ErrorMessage message={firstNameErrorMessage} show={firstNameError} />
 
         {/* last name input */}
         <div className="input-wrap">
           <label>{"Last Name"}:</label>
-          <input placeholder="Baggins" />
+          <input
+            placeholder="Baggins"
+            onChange={(e) => {
+              this.updateStateOnSingleInputs("lastName", e.target.value);
+              this.errorCheckInput("lastName", e.target.value);
+            }}
+          />
         </div>
-        <ErrorMessage message={lastNameErrorMessage} show={true} />
+        <ErrorMessage message={lastNameErrorMessage} show={lastNameError} />
 
         {/* Email Input */}
         <div className="input-wrap">
           <label>{"Email"}:</label>
-          <input placeholder="bilbo-baggins@adventurehobbits.net" />
+          <input
+            placeholder="bilbo-baggins@adventurehobbits.net"
+            onChange={(e) => {
+              this.updateStateOnSingleInputs("email", e.target.value);
+              this.errorCheckInput("email", e.target.value);
+            }}
+          />
         </div>
-        <ErrorMessage message={emailErrorMessage} show={true} />
+        <ErrorMessage message={emailErrorMessage} show={emailError} />
 
         {/* City Input */}
         <div className="input-wrap">
           <label>{"City"}:</label>
-          <input placeholder="Hobbiton" />
+          <input
+            placeholder="Hobbiton"
+            list="cities"
+            onChange={(e) => {
+              this.updateStateOnSingleInputs("city", e.target.value);
+              this.errorCheckInput("city", e.target.value);
+            }}
+          />
+          <datalist id="cities">
+            {allCities.map((city) => (
+              <option key={city}>{city}</option>
+            ))}
+          </datalist>
         </div>
-        <ErrorMessage message={cityErrorMessage} show={true} />
+        <ErrorMessage message={cityErrorMessage} show={cityError} />
 
         <div className="input-wrap">
           <label htmlFor="phone">Phone:</label>
@@ -56,7 +199,10 @@ export class ClassForm extends Component {
           </div>
         </div>
 
-        <ErrorMessage message={phoneNumberErrorMessage} show={true} />
+        <ErrorMessage
+          message={phoneNumberErrorMessage}
+          show={phoneNumberError}
+        />
 
         <input type="submit" value="Submit" />
       </form>
