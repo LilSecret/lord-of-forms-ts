5e0700c4725bb696384007521b304285ddb87396
Charles <chuckm248@gmail.com>
2024-04-06 21:52:31 -0600

    feat: build a fn to format input label

--- src/utils/transformations.ts
+++ src/utils/transformations.ts
@@ -1,3 +1,6 @@
+import { TSingleInputs } from "../ClassApp/ClassTextInput";
+import { TInput } from "../types";
+
 export const capitalize = (word: string) => {
   return (
     word.slice(0, 1).toUpperCase() + word.slice(1, word.length).toLowerCase()
@@ -6,6 +9,20 @@ export const capitalize = (word: string) => {
 
 export const toUpperCase = (word: string) => {
   return word.toUpperCase();
+};
+
+export const formatInputLabel = (word: string) => {
+  const words = word.split(" ");
+  let label;
+  if (words.length > 1) {
+    label =
+      words[0].toLowerCase() +
+      words[1].charAt(0).toUpperCase() +
+      words[1].slice(1).toLowerCase();
+  } else {
+    label = words[0].toLowerCase();
+  }
+  return label as keyof TSingleInputs;
 };
 
 export const formatPhoneNumber = (number: string) => {
